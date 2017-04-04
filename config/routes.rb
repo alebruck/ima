532dbcbbfbc78b6fde83fb4895e2dadcc050e7b2
Alessandro Diogo Bruckheimer <alebruck@gmail.com>
2017-04-04 19:11:40 -0300

    Set up Action Cable
    https://github.com/rails/rails/tree/master/actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  mount ActionCable.server => '/cable'
+
   get :signin, to: 'sessions#new', as: :new_user_session
   delete :signout, to: 'sessions#destroy', as: :destroy_user_session
   post :signin, to: 'sessions#create', as: :user_session
